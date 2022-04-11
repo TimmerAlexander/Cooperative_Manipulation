6a2aa861373f84121d244115520c190528b97118
TimmerAlexander <78174036+TimmerAlexander@users.noreply.github.com>
2022-04-11 14:02:19 +0200

    Safety commit

--- cooperative_manipulation_controllers/scripts/cooperative_manipulation_controllers/ur16e_admittance_control.py
+++ cooperative_manipulation_controllers/scripts/cooperative_manipulation_controllers/ur16e_admittance_control.py
@@ -1,105 +1,248 @@
 #!/usr/bin/env python3
+
+# /***************************************************************************
+# *Admittance controller
+
+# * Input: 
+# * desired cartesian velocity from path planning: self.desired_velocity (In 'base_link' frame)
+# * external wrench from the f/t sensor: self.wrench_ext (In 'tool0' frame)
+
+# * Output: 
+# * target joint velocity: self.target_joint_velocity (In 'base_link' frame)
+# **************************************************************************/
+
 import rospy
-from geometry_msgs.msg import WrenchStamped, Twist
 import tf
-
-import threading
-
 import moveit_commander
-import moveit_msgs.msg
-from sensor_msgs.msg import JointState
+from geometry_msgs.msg import WrenchStamped, Vector3Stamped
 from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
+
 import numpy
 
-#J_ur=self.group.get_jacobian_matrix(joint_states_array)
+#J_ur=self.group.get_jacobian_matrix(self.current_joint_states_array)
 
 class ur_admittance_controller():
     
-    
+    def config(self):
+        # Damping gains
+        self.D_x = 10
+        self.D_y = 10
+        self.D_z = 10
+        # Stiffness gains
+        self.C_x = 1.0
+        self.C_y = 1.0
+        self.C_z = 1.0
+        # Set min and max limits for the cartesian velocity
+        self.cartesian_velocity_min_limit = 0.001
+        self.cartesian_velocity_max_limit = 0.1
+        # Set publish rate
+        self.publish_rate = 100 # Hz
+        # Initialize wrench_filtered
+        self.wrench_filtered_x = 0.0
+        self.wrench_filtered_y = 0.0
+        self.wrench_filtered_z = 0.0
+        # Wrench filter parameter
+        self.wrench_filter = 0.03
+        
+        # ? Brauche ich das? --------------------------------------------
+        self.velocity_threshhold = 0.1
+        self.cmd_vel_filter = 0.05
+        # ? --------------------------------------------------------------------------
+        
     def __init__(self):
         # Load config
         self.config()
-        # Initialize node
+        
+        # *Initialize the msgs:
+        # Initialize target cartesian velocity array
+        self.target_cartesian_velocity = numpy.array([0.0,0.0,0.0,0.0,0.0,0.0])
+        # Initialize shutdown joint velocity
+        self.shutdown_joint_velocity = Float64MultiArray()
+        self.shutdown_joint_velocity.data = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        # Declare target joint velocity msgs
+        self.target_joint_velocity = Float64MultiArray()
+        
+        self.tool0_cartesian_velocity = Vector3Stamped()
+        
+        self.velocity_transformed = numpy.array([0.0,0.0,0.0,0.0,0.0,0.0])
+        
+        
+        # Initialize desired velocity
+        self.desired_velocity = numpy.array([0.0,0.01,0.0,0.0,0.0,0.0])
+        
+        # * Initialize node
         rospy.init_node('admittance_controller_node', anonymous=True)
         rospy.loginfo("admittance controller running")
-        # Get namespace from launch file
+        
+        # Initialize on_shutdown methode
+        rospy.on_shutdown(self.shutdown)
+
+        # * Get namespace from launch file
         self.namespace = rospy.get_param("~ur_ns")
-        # Initialize move_it
-        self.joint_vel = Float64MultiArray()
+        
+        # * Initialize move_it
         group_name = 'manipulator'
         print("Initialize movit_commander. Group name: ",group_name)
         self.group = moveit_commander.MoveGroupCommander(group_name)
-        # Initialize publisher
-        self.pub = rospy.Publisher("/" + self.namespace + "/ur_admittance_controller/command", Float64MultiArray, queue_size=1)
         
+        # * Initialize publisher:
+        # Publish final joint velocity to "/ur/ur_admittance_controller/command"
+        self.joint_velocity_pub = rospy.Publisher("/" + self.namespace + "/ur_admittance_controller/command", Float64MultiArray, queue_size=1)
+        
+        # * Initialize subscriber:
+        # Subscriber to "/ur/wrench"
+        self.wrench_ext_sub = rospy.Subscriber("/" + self.namespace + "/wrench", WrenchStamped, self.wrench_cb)
+        
+        # * Initialize tf TransformListener
         self.listener = tf.TransformListener()
-        now = rospy.Time()
-        self.listener.waitForTransform("base_link", "tool0", rospy.Time(), rospy.Duration(4.0))
-        (self.initial_position,self.initial_orientation) = self.listener.lookupTransform('base_link', 'tool0', now)
-        # Print initial position and orientation of 'tool0'
-        print("Initial position: ",self.initial_position)
-        print("Initial orientation: ",self.initial_orientation)
-        # Desiered position and orientation of 'tool0'
-        self.goal_position = self.initial_position
-        self.goal_orientation = self.initial_orientation
-        
-        # Get '/ur/wrench'
-        rospy.Subscriber("/" + self.namespace + "/wrench", WrenchStamped, self.wrench_cb)
-        
-        #rospy.spin()
-        
-        # start controller thread
-        ctrl_thread = threading.Thread(target=control_thread, args = [self.publish_rate])
-        ctrl_thread.start()
-        
-        
+        self.listener.waitForTransform("tool0","base_link", rospy.Time(), rospy.Duration(4.0))
 
-    def control_thread(rate):
-        """Control loop
-         """
-        print("test")
+        # Run publish_joint_velocity_thread
+        self.publish_joint_velocity_thread()
+        
+        rospy.spin()
+    
+    
+    def wrench_cb(self,wrench_ext):
+        """ Get external wrench.
+        """
+        print("wrench_ext:")
+        print(wrench_ext)
+        
+        #? Wozu das? ---------------------------------------------------------------------------------------------
+        #self.wrench_filtered_x = self.wrench_filtered_x*(1-self.wrench_filter) + wrench_ext.wrench.force.x * self.wrench_filter 
+        #self.wrench_filtered_y = self.wrench_filtered_y*(1-self.wrench_filter) + wrench_ext.wrench.force.y * self.wrench_filter 
+        #self.wrench_filtered_z = self.wrench_filtered_z*(1-self.wrench_filter) + wrench_ext.wrench.force.z * self.wrench_filter 
+        #? -------------------------------------------------------------------------------------------------------
+        
+    
+    # ? In welchen Koordiantensystem wird die Bahn berechnet?
+    def transform_velocity(self,cartesian_velocity: numpy.array):
+        """ Transform the cartesian velocity from the 'tool0' frame to the 'base_link' frame
+        """
+        now = rospy.Time()
+        
+        self.tool0_cartesian_velocity.header.frame_id = 'tool0'
+        self.tool0_cartesian_velocity.header.stamp = now
+        self.tool0_cartesian_velocity.vector.x = cartesian_velocity[0]
+        self.tool0_cartesian_velocity.vector.y = cartesian_velocity[1]
+        self.tool0_cartesian_velocity.vector.z = cartesian_velocity[2]
+        
+        self.base_link_cartesian_velocity = self.listener.transformVector3('base_link',self.tool0_cartesian_velocity)
+        
+        self.velocity_transformed = numpy.array([self.base_link_cartesian_velocity.vector.x,self.base_link_cartesian_velocity.vector.y,self.base_link_cartesian_velocity.vector.z,0.0,0.0,0.0])
+        
+        print("self.velocity_transformed")
+        print(self.velocity_transformed)
+        return self.velocity_transformed
+        
+    def publish_joint_velocity_thread(self):
+        """ This thread calculates and publishes the target joint velocity.
+        """
+        rate = rospy.Rate(self.publish_rate)
         while not rospy.is_shutdown():
+            
+            print("desired_velocity: ")
+            print(self.desired_velocity)
+            
+            self.desired_velocity_transformed = self.transform_velocity(self.desired_velocity)
 
- 
+            #  Todo: Uncomment in ready code
+            # Calculate the target cartesian velocity using the admittance
+            #self.target_cartesian_velocity[0] = self.desired_velocity[0] + self.wrench_filtered_x * pow(self.D_x,-1)
+            #self.target_cartesian_velocity[1] = self.desired_velocity[1] + self.wrench_filtered_y * pow(self.D_y,-1)
+            #self.target_cartesian_velocity[2] = self.desired_velocity[2] + self.wrench_filtered_z * pow(self.D_z,-1)
+
+            # * Placeholder--------------------------------------------------------
+            #  Todo: Delete in ready code
+            self.target_cartesian_velocity[0] = self.desired_velocity_transformed[0] + self.wrench_filtered_x * pow(self.D_x,-1)
+            self.target_cartesian_velocity[1] = self.desired_velocity_transformed[1] + self.wrench_filtered_y * pow(self.D_y,-1)
+            self.target_cartesian_velocity[2] = self.desired_velocity_transformed[2] + self.wrench_filtered_z * pow(self.D_z,-1)
+            # * Placeholder--------------------------------------------------------
+
+
+            # Check for cartesian velocity min limit
+            if abs(self.target_cartesian_velocity[0]) < self.cartesian_velocity_min_limit:
+                self.target_cartesian_velocity[0] = 0.0
+            if abs(self.target_cartesian_velocity[1]) < self.cartesian_velocity_min_limit:
+                self.target_cartesian_velocity[1] = 0.0
+            if abs(self.target_cartesian_velocity[2]) < self.cartesian_velocity_min_limit:
+                self.target_cartesian_velocity[2] = 0.0
+            
+            # Check for cartesian velocity max limit
+            if abs(self.target_cartesian_velocity[0]) > self.cartesian_velocity_max_limit:
+                # Check for sign
+                if numpy.sign(self.target_cartesian_velocity[0]) == 1:
+                    self.target_cartesian_velocity[0] = self.cartesian_velocity_max_limit
+                else:
+                    self.target_cartesian_velocity[0] = -self.cartesian_velocity_max_limit
+            if abs(self.target_cartesian_velocity[1]) > self.cartesian_velocity_max_limit:
+                # Check for sign
+                if numpy.sign(self.target_cartesian_velocity[1]) == 1:
+                    self.target_cartesian_velocity[1] = self.cartesian_velocity_max_limit
+                else:
+                    self.target_cartesian_velocity[1] = -self.cartesian_velocity_max_limit
+            if abs(self.target_cartesian_velocity[2]) > self.cartesian_velocity_max_limit:
+                # Check for sign
+                if numpy.sign(self.target_cartesian_velocity[2]) == 1:
+                    self.target_cartesian_velocity[2] = self.cartesian_velocity_max_limit
+                else:
+                    self.target_cartesian_velocity[2] = -self.cartesian_velocity_max_limit
+            
+            #print("target_cartesian_velocity")
+            #print(self.target_cartesian_velocity)
+            
+            # Get current joint states and calculate the jaobian-matrix
+            self.current_joint_states_array = self.group.get_current_joint_values()       
+            self.jacobian = self.group.get_jacobian_matrix(self.current_joint_states_array) 
+            self.inverse_jacobian = numpy.linalg.inv(self.jacobian)
+
+            # Calculate the target joint velocity 
+            self.target_joint_velocity.data = self.inverse_jacobian.dot(self.target_cartesian_velocity)
+            #print("target_joint_velocity: ")
+            #print(self.target_joint_velocity)
+            
+            # Publish the target_joint_velocity
+            self.joint_velocity_pub.publish(self.target_joint_velocity)
+            
+            # Sleep for publish_rate
             rate.sleep()
+
+    def shutdown(self):
+        """ This function is called rospy.on_shutdown!
+        """
         
+        print("Shutdown amittcance controller!")
         
-    def wrench_cb(self,F_ext):
+        print("Send stop joint velocity!")
+        self.joint_velocity_pub.publish(self.shutdown_joint_velocity)
         
-        # F_ext == external force
-        print("F_ext:")
-        print(F_ext)
-
-        now = rospy.Time()
-        (curr_position,curr_orientation) = self.listener.lookupTransform('base_link', 'tool0', now)
-
-        self.position_diff_x = self.goal_position[0] - curr_position[0]
-        self.position_diff_y = self.goal_position[1] - curr_position[1]
-        self.position_diff_z = self.goal_position[2] - curr_position[2]
+        print("Unregister from joint_velocity_pub!")
+        self.joint_velocity_pub.unregister()
         
-        if self.position_diff_x < self.position_diff_limit:
-            self.position_diff_x = 0
-        if self.position_diff_y < self.position_diff_limit:
-            self.position_diff_y = 0
-        if self.position_diff_z < self.position_diff_limit:
-            self.position_diff_z = 0
+        print("Unregister from wrench_ext_sub!")
+        self.wrench_ext_sub.unregister()
+    
+if __name__ == '__main__':
+    ur_admittance_controller()
+    
+    
+   # def joint_state_cb(self,joint_states):
+    #    """ Get current joint velocity.
+     #   """
+      #  current_joint_velocity_array =[joint_states.velocity[0],joint_states.velocity[1],joint_states.velocity[2],joint_states.velocity[3],joint_states.velocity[4],joint_states.velocity[5]]
+        #print("current_joint_velocity_array: ")
+        #print(current_joint_velocity_array)
+        
+        # ? Brauche ich das? -------------------------------------
+     #   joint_states_array=[joint_states.position[0],joint_states.position[1],joint_states.position[2],joint_states.position[3],joint_states.position[4],joint_states.position[5]]
+      
+        
+     #   self.J_ur=self.group.get_jacobian_matrix(joint_states_array)
+        # ? ------------------------------------------------------
 
-        print("self.position_diff_x,self.position_diff_y,self.position_diff_z")
-        print(self.position_diff_x,self.position_diff_y,self.position_diff_z)
-
-        # Force difference
-        self.F_diff_x = self.F_d_x*(1-self.wrench_filter) + F_ext.wrench.force.x * self.wrench_filter 
-        self.F_diff_y = self.F_d_y*(1-self.wrench_filter) + F_ext.wrench.force.y * self.wrench_filter 
-        self.F_diff_z = self.F_d_z*(1-self.wrench_filter) + F_ext.wrench.force.z * self.wrench_filter 
-
-        print("F_diff_x",self.F_diff_x)
-        print("F_diff_y",self.F_diff_y)
-        print("F_diff_z",self.F_diff_z)
-
-        self.cmd_vel.linear.x = (self.F_diff_x - self.position_diff_x * self.C_x) * pow(self.D_x,-1)
-        self.cmd_vel.linear.y = (self.F_diff_y - self.position_diff_y * self.C_y) * pow(self.D_y,-1)
-        self.cmd_vel.linear.z = (self.F_diff_z - self.position_diff_z * self.C_z) * pow(self.D_z,-1)
-
+# ? Was ist das?--------------------------------------------------------
 #        self.cmd_vel_filtered.linear.x = self.cmd_vel_filtered.linear.x * (1-self.cmd_vel_filter) + self.cmd_vel.linear.x * self.cmd_vel_filter
 #        self.cmd_vel_filtered.linear.y = self.cmd_vel_filtered.linear.y * (1-self.cmd_vel_filter) + self.cmd_vel.linear.y * self.cmd_vel_filter
 #        self.cmd_vel_filtered.linear.z = self.cmd_vel_filtered.linear.z * (1-self.cmd_vel_filter) + self.cmd_vel.linear.z * self.cmd_vel_filter
@@ -114,68 +257,7 @@ class ur_admittance_controller():
 #        if abs(self.cmd_vel_filtered.linear.z)> self.velocity_threshhold:
 #            self.cmd_vel_filtered.linear.z = self.cmd_vel_filtered.linear.z/abs(self.cmd_vel_filtered.linear.z) * self.velocity_threshhold
 
-        #print("Publish Twist:",self.cmd_vel.linear.x,self.cmd_vel.linear.y)
-        #print(self.position_diff_z)
-#------------------------------------------------------------------------
-        print("cmd_vel: ")
-        print("self.cmd_vel.linear.x: ",self.cmd_vel.linear.x)
-        print("self.cmd_vel.linear.y: ",self.cmd_vel.linear.y)
-        print("self.cmd_vel.linear.z: ",self.cmd_vel.linear.z)
-        print("self.cmd_vel.angular.x: ",self.cmd_vel.angular.x)
-        print("self.cmd_vel.angular.y: ",self.cmd_vel.angular.y)
-        print("self.cmd_vel.angular.z: ",self.cmd_vel.angular.z)
-        
-        # Save the cmd_vel in an array
-        self.target_vel = numpy.array([self.cmd_vel.linear.x,self.cmd_vel.linear.y,self.cmd_vel.linear.z,self.cmd_vel.angular.x,self.cmd_vel.angular.y,self.cmd_vel.angular.z])
-        
-        #print("Publish self.target_vel: ")
-        #print(self.target_vel)
-        
-    
-        joint_states_array = self.group.get_current_joint_values()
-        self.Jacobian_ur = self.group.get_jacobian_matrix(joint_states_array)
-        
-        print("Print Jacobian_ur: ",self.Jacobian_ur)
 
-        inverse = numpy.linalg.inv(self.Jacobian_ur)
-        self.target_dq = inverse.dot(self.target_vel)
-        self.joint_vel.data = self.target_dq
-        print("Joint Velocity: ")
-        print(self.target_dq)
-        
-        
-        print("self.joint_vel.data: ",self.joint_vel.data)
-        self.pub.publish(self.joint_vel)
-#--------------------------------------------------------------------
-        #print("Publish Twist: ")
-        #print(self.cmd_vel)
-        #self.pub.publish(self.cmd_vel)
-    
 
-    def config(self):
-        # Damping gains
-        self.D_x = 0.1
-        self.D_y = 0.1
-        self.D_z = 0.1
-        # Stiffness gains
-        self.C_x = 1.0
-        self.C_y = 1.0
-        self.C_z = 1.0
-        # Desired force 
-        self.F_d_x = 0.0
-        self.F_d_y = 0.0
-        self.F_d_z = 0.0
-        # Force difference
-        
-        self.position_diff_limit = 0.001
-        self.publish_rate = 100
-        # --------------------------
-        self.cmd_vel = Twist()
-        self.velocity_threshhold = 0.1
-        self.cmd_vel_filtered = Twist()
-        self.wrench_filter = 0.03
-        self.cmd_vel_filter = 0.05
 
-    
-if __name__ == '__main__':
-    ur_admittance_controller()
+
