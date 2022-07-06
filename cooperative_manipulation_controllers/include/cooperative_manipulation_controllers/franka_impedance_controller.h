40c6407042a013e61f0c80b5d61f8f5fe6b14629
TimmerAlexander <timmer.alexander@hotmail.com>
2022-07-06 13:39:10 +0200

    Refactor franka_impedance_controller.h

--- cooperative_manipulation_controllers/include/cooperative_manipulation_controllers/franka_impedance_controller.h
+++ cooperative_manipulation_controllers/include/cooperative_manipulation_controllers/franka_impedance_controller.h
@@ -9,19 +9,15 @@
 #include <controller_interface/multi_interface_controller.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/robot_hw.h>
-#include <realtime_tools/realtime_publisher.h>
 #include <ros/node_handle.h>
-#include <ros/time.h>
 
 
-#include <franka_example_controllers/JointTorqueComparison.h>
 #include <franka_hw/franka_cartesian_command_interface.h>
 #include <franka_hw/franka_model_interface.h>
 #include <franka_hw/trigger_rate.h>
 
-#include <moveit/move_group_interface/move_group_interface.h>
 #include <std_msgs/Float64MultiArray.h>
-#include <geometry_msgs/Twist.h>
+
 
 namespace cooperative_manipulation_controllers {
 
@@ -35,6 +31,12 @@ class FrankaImpedanceController : public controller_interface::MultiInterfaceCon
   void velocityCmdCallback(const std_msgs::Float64MultiArray::ConstPtr& vel_cmd);
 
  private:
+   std::vector<hardware_interface::JointHandle> velocity_joint_handles_;
+  franka_hw::FrankaVelocityCartesianInterface* velocity_cartesian_interface_;
+  std::unique_ptr<franka_hw::FrankaCartesianVelocityHandle> velocity_cartesian_handle_;
+  std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
+  std::vector<hardware_interface::JointHandle> joint_handles_;
+
   ros::Subscriber sub_velocity_command_;
 
   std::array<double, 6>  vel_current_ = {0.0,0.0,0.0,0.0,0.0,0.0};
@@ -45,19 +47,12 @@ class FrankaImpedanceController : public controller_interface::MultiInterfaceCon
       const std::array<double, 7>& tau_d_calculated,
       const std::array<double, 7>& tau_J_d);  // NOLINT (readability-identifier-naming)
 
-  std::vector<hardware_interface::JointHandle> velocity_joint_handles_;
-  franka_hw::FrankaVelocityCartesianInterface* velocity_cartesian_interface_;
-  std::unique_ptr<franka_hw::FrankaCartesianVelocityHandle> velocity_cartesian_handle_;
-  std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
-  std::vector<hardware_interface::JointHandle> joint_handles_;
-
   static constexpr double kDeltaTauMax{1.0};
-  double vel_acc_{0.0001};
+  double vel_acc_{0.001};
   std::vector<double> k_gains_;
   std::vector<double> d_gains_;
   double coriolis_factor_{1.0};
   std::array<double, 7> dq_filtered_;
-  std::array<double, 16> initial_pose_;
 
   franka_hw::TriggerRate rate_trigger_{1.0};
   std::array<double, 7> last_tau_d_{};
